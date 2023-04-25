a19f369ed6f2705d9ee1acd8ec9f965b85a716bb
Alex <alexandre.kovalchuk@gmail.com>
2023-04-25 13:54:28 +0300

    working with classes

--- My repositories/js/script.js
+++ My repositories/js/script.js
@@ -1,17 +1,36 @@
-let data;
 const row = document.querySelector('.row');
+class GithubAPI {
+   constructor(token, username) {
+      this.token = token;
+      this.username = username;
+   }
 
-fetch('https://api.github.com/users/Alexandre-Kovalchuk/repos')
-   .then(response => response.json())
-   .then(data => {
-      data.slice(1, 5).forEach(function (elem) {
-         let html = `
-            <div class="row__item">
-               <div class="row__name">${elem.full_name}</div>
-               <div class="row__html"><a href="${elem.html_url}">${elem.html_url}</a></div>
-               <div class="row__description"></div>
-            </div>`;
-         row.innerHTML += html;
+   async getRepos() {
+      const response = await fetch(`https://api.github.com/users/${this.username}/repos`, {
+         headers: {
+            'Authorization': `token ${this.token}`
+         }
       });
-   });
+      const repos = await response.json();
+      return repos;
 
+   }
+}
+
+async function result() {
+   const github = new GithubAPI('my token', 'Alexandre-Kovalchuk');
+   const repos = await github.getRepos();
+   console.log(repos);
+
+   for (let elem of repos) {
+      let html = `
+   <div class="row__item">
+      <div class="row__name">${elem.full_name}</div>
+      <div class="row__html"><a href="${elem.html_url}">${elem.html_url}</a></div>
+      <div class="row__description"></div>
+   </div>`;
+      row.innerHTML += html;
+   }
+}
+
+result();
